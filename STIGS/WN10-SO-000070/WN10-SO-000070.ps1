ee4e3a7987e8b75adf0414727e36762a804c0adc
daniel-667 <148985921+daniel-667@users.noreply.github.com>
2025-03-19 18:26:48 -0700

    Update WN10-SO-000070.ps1

--- STIGS/WN10-SO-000070/WN10-SO-000070.ps1
+++ STIGS/WN10-SO-000070/WN10-SO-000070.ps1
@@ -1,1 +1,44 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
 
+.NOTES
+    Author          : Daniel Avila
+    LinkedIn        : linkedin.com/in/avdaniel/
+    GitHub          : github.com/daniel-667
+    Date Created    : 2025-03-19
+    Last Modified   : 2025-03-19
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000070
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+ 
+#>
+
+
+# Define registry path and values
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$regName = "InactivityTimeoutSecs"
+$regValue = 900
+
+# Check if the registry path exists, create if not
+if (!(Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the registry value
+Set-ItemProperty -Path $regPath -Name $regName -Value $regValue -Type DWord
+
+# Verify the change
+$setValue = Get-ItemProperty -Path $regPath -Name $regName
+if ($setValue.$regName -eq $regValue) {
+    Write-Host "Machine inactivity limit set to 15 minutes successfully." -ForegroundColor Green
+} else {
+    Write-Host "Failed to set machine inactivity limit." -ForegroundColor Red
+}
