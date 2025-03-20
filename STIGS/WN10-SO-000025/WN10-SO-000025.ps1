9315d682a4c55947ee88078ca94bc0cce29899c2
daniel-667 <148985921+daniel-667@users.noreply.github.com>
2025-03-19 19:14:04 -0700

    Update WN10-SO-000025.ps1

--- STIGS/WN10-SO-000025/WN10-SO-000025.ps1
+++ STIGS/WN10-SO-000025/WN10-SO-000025.ps1
@@ -1,1 +1,45 @@
 
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
+
+.NOTES
+    Author          : Daniel Avila
+    LinkedIn        : linkedin.com/in/avdaniel/
+    GitHub          : github.com/daniel-667
+    Date Created    : 2025-03-19
+    Last Modified   : 2025-03-19
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000025
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+ 
+#>
+
+# Define the new name for the guest account
+$newGuestName = "DisabledGuest"
+
+# Get the guest account object
+$guestAccount = Get-WmiObject -Class Win32_UserAccount -Filter "SID LIKE 'S-1-5-%-501'"
+
+if ($guestAccount) {
+    # Rename the guest account
+    Rename-LocalUser -Name $guestAccount.Name -NewName $newGuestName
+    
+    # Confirm the change
+    $renamedAccount = Get-WmiObject -Class Win32_UserAccount -Filter "Name = '$newGuestName'"
+    
+    if ($renamedAccount) {
+        Write-Host "Guest account renamed to '$newGuestName' successfully." -ForegroundColor Green
+    } else {
+        Write-Host "Failed to rename the guest account." -ForegroundColor Red
+    }
+} else {
+    Write-Host "No guest account found on this machine." -ForegroundColor Yellow
+}
